424d8eb7e10b14f8a936236e6334f4f35b692a3b
Julia Kastrup <julia.kastrup@thoughtworks.com>
2021-06-15 16:58:57 -0300

    Cria novo exercício com condicional

---
+++ ExJulia.py
@@ -0,0 +1,13 @@
+a = input('Digite o nome do cachorro mais bonito da Vila Anglo Brasileira: ')
+
+if a == "Alicio" or a == "Aliço" or a == "Lilico" or a == "Alício":
+    print('Parabéns! Você acertou!')
+else:
+    print('Tente novamente!')
+
+
+
+
+    
+
+
