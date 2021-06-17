2fdf29c4df0ca1c9715de495c95d64ebf72127c3
Gabriel Cellier <gabrielcellier@gmail.com>
2021-06-17 16:55:32 -0300

    .

---
+++ Ex020.py
@@ -0,0 +1,9 @@
+#Ler 4 nomes e sortear uma ordem entre os nomes dados
+from random import shuffle
+a1 = str(input('Digite o nome do primeiro aluno: '))
+a2 = str(input('Digite o nome do segundo aluno: '))
+a3 = str(input('Digite o nome do terceiro aluno: '))
+a4 = str(input('Digite o nome do quarto aluno: '))
+lista = [a1, a2, a3, a4]
+shuffle(lista)
+print ('A lista e sua ordem será composta por {}'.format(lista))
