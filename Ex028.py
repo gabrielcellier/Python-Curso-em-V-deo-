39083dc252df3e801273a3adb8b8c7127a9a2142
Gabriel Cellier <gabrielcellier@gmail.com>
2021-06-21 21:35:17 -0300

    Novos exercícios

--- Ex028.py
+++ Ex028.py
@@ -1,1 +1,10 @@
 #Computador sorteia um numero de 0 a 5 e a pessoa tenta descobrir qual é. Avisar se acertou ou errou
+import random
+num = int(input('Tente adivinha o número de 0 a 5 que o computador irá sortear: '))
+lista = [0, 1, 2, 3, 4, 5]
+sorteio = random.choice(lista)
+if sorteio == num:
+    print ('Parabéns o número digitado foi {} e o sorteado foi {}.'.format(num, sorteio))
+else:
+    print ('Infelizmente você errou pois o número digitado foi {} e o sorteado foi {}'.format(num, sorteio))
+
