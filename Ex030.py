788d7b0682c17b9229260606a2261cf110270c3d
Gabriel Cellier <gabrielcellier@gmail.com>
2021-06-21 22:48:26 -0300

    Novos exercícios

--- Ex030.py
+++ Ex030.py
@@ -1,1 +1,8 @@
 #Ler um número dado e avisar se ele é par ou ímpar
+num = int(input('Digite um número inteiro qualquer: '))
+conta = num % 2
+if conta == 0:
+    print ('O número digitado {} é par'.format(num))
+else:
+    print ('O número digitado {} é ímpar'.format(num))
+#o '% 2' poderia ser colocado na variável num mas como quis mostrar ele nos prints criei a variável conta
--- Ex032.py
+++ Ex032.py
@@ -1,1 +1,10 @@
 #Ler um ano e avisar se ele é bissexto
+ano = int(input('Digite um ano para saber se ele é bissexto: '))
+c1 = ano % 4
+c2 = c1 % 100
+c3 = c2 % 400
+if c3 == 0:
+    print ('O ano de {} é um ano bissexto.'.format(ano))
+else:
+    print ('O ano de {} não é um ano bissexto.'.format(ano))
+
