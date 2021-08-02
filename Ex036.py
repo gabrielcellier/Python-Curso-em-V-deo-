0dc3a2fdd8c4e8e946d26fdc4f7010637ca622ca
Gabriel Cellier <gabrielcellier@gmail.com>
2021-08-02 18:27:24 -0300

    Exercícios 36-45 (Mundo 2 - aula 12)

--- Ex036.py
+++ Ex036.py
@@ -1,14 +1,10 @@
 #perguntar I) valor da casa, II) salário do comprador, III) quantos anos pra pagar
 #Calcular o valor da prestação mensal, não pode exceder 30% do salário ou o empréstimo será negado
 casa = float(input('Qual o valor da casa? R$ '))
-salario = float(input('Qual o seu salário atualmente? R$ '))
-prazo = int(input('Quantos anos para pagar o imóvel? '))
-prestacao = casa / (prazo * 12)
-
-if prestacao >= (salario * 0.30):
-    input ('Como a prestação no valor de R${:.2f} é maior que 30% do salário de R$ {} '
-           'o empréstimo será \033[1mnegado\033[m'.format(prestacao, salario))
+salario = float(input('Qual o salário do comprador? R$ '))
+tempo = int(input('Quantos anos para pagar a casa? '))
+prestacao = casa // (tempo * 12)
+if (salario * 0.30) > prestacao:
+    print ('Como o valor da prestação (R${:.2f}) não ultrapassa 30% do salário o empréstimo foi aceito.'.format(prestacao))
 else:
-    input ('Como a prestação no valor de R${:.2f} é menor que 30% do salário de R$ {} '
-           'o empréstimo será \033[1maceito\033[m'.format(prestacao, salario))
-#
+    print ('Como o valor da prestação (R${:.2f}) ultrapassa 30% do salário o empréstimo foi negado.'.format(prestacao))
