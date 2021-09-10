9add938a0a5f073f2ac83f5dff896044010e0bbe
Gabriel Cellier <gabrielcellier@gmail.com>
2021-09-10 19:57:56 -0300

    Exercícios 46-56 (Mundo 2 - aula 13)

--- Ex055.1.py
+++ Ex055.1.py
@@ -1,8 +1,8 @@
 #ler peso de 5 pessoas e mostrar qual o maior e o menor
 lista = []
 for p in range (1, 6):
-    peso = float(input('Digite o peso da {}ª pessoa (em kg): '.format(p)))
-    lista = lista + [peso] #adicionaremos os 'peso' nessa lista 'lista'
-print('O maior peso foi {:.2f} kg e o menor foi {:.2f} kg.'.format(max(lista), min(lista)))
+    peso = float(input('Digite o peso em kg da {}ª pessoa: '.format(p)))
+    lista += [peso]
+print ('O maior peso escrito foi de {:.1f} kg e o menor de {:.1f} kg.'.format(max(lista), min(lista)))
 #nesse modo criamos uma lista que será atualizada com todos os valores de 'peso' inseridos
 #usando max() e min() selecionaremos os maiores e menores valores dessa lista
--- Ex055.py
+++ Ex055.py
@@ -1,17 +1,10 @@
 #ler peso de 5 pessoas e mostrar qual o maior e o menor
 maior = 0
 menor = 0
-for p in range (1, 6):
-    peso = float(input('Digite o peso da {}ª pessoa (em kg): '.format(p)))
-    if p == 1:   #aqui definimos o primeiro valor como maior e menor
-        maior = p
-        menor = p
-    else:  #aqui definimos que os seguintes 'p' (2-6) substituirão o maior e menor
-        if peso > maior:   #se o peso dos 'p' seguintes for maior substituirá o 'maior' atual
-            maior = peso
-        if peso < menor:    #se o peso dos 'p' seguintes for menor substituirá o 'menor' atual
-            menor = peso
-print ('O maior peso foi {:.2f} kg e o menor {:.2f} kg.'.format(maior, menor))
-#nesse método o primeiro valor dado é definido tanto como maior e menor, e os outros serão testados e caso
-#sejam maiores ou menores substituiram o valor do 'p' anterior definido como 'maior' ou 'menor', respectivamente.
-
+for p in range(1, 6):
+    peso = float(input('Digite o peso (em kg) da {}ª pessoa: '.format(p)))
+    if peso > maior or maior == 0: #se o valor for maior que o maior atual ou que 0 (o maior inicial), substitui ele
+        maior = peso
+    if peso < menor or menor == 0: #se o valor for menor que o emnor atual ou que 0 (menor inicial), substitui ele.
+        menor = peso
+print ('O maior peso escrito foi de {:.1f} kg e o menor de {:.1f} kg.'.format(maior, menor))
