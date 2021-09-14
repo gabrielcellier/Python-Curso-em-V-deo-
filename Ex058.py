0a3b550eb41ebb4e03670cdb51437ce69b3d229f
Gabriel Cellier <gabrielcellier@gmail.com>
2021-09-13 23:54:06 -0300

    Exercícios 57-65 (Mundo 2 - aula 14)

--- Ex058.py
+++ Ex058.py
@@ -3,10 +3,18 @@
 from random import randint
 tentativa = 0
 sorteio = randint(0, 10)
+
 print ('Olá, jogaremos um jogo de adivinhação, onde você chutará o número que eu estou pensando!')
 n = int(input('Qual número entre 0 a 10 que eu estou pensando? '))
+
 while n != sorteio:
-    n = int(input('Errou! Digite outro número de 0 a 10 que estou pensando: '))
+    if n > sorteio:
+        print ('O valor digitado é maior que o escolhido pelo computador.')
+    if sorteio > n:
+        print ('O valor digitado é menor que o escolhido pelo computador.')
+    n = int(input('Como você errou digite outro número de 0 a 10 que estou pensando: '))
     tentativa += 1
-print ('Acertou! O número que pensei foi o {}. Você tentou {} vezes até conseguir acertar.'
-       .format(sorteio, tentativa))
+print('Acertou! O número que pensei foi o {}. Você tentou {} vezes até conseguir acertar.'
+      .format(sorteio, tentativa))
+
+
