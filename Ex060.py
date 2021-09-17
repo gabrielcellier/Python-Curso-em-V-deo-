39b3c6abe240433632d90345678faba78bd5744e
Gabriel Cellier <gabrielcellier@gmail.com>
2021-09-17 16:25:18 -0300

    Exercícios 57-65 (Mundo 2 - aula 14)

---
+++ Ex060.py
@@ -0,0 +1,10 @@
+#programa ler um número e mostrar o seu fatorial
+n = int(input('Digite um número para calcularmos seu fatorial: '))
+c = n
+f = 1
+while c > 0:
+    f *= c
+    c -= 1
+print ('fatorial de {} é {}'.format(n, f))
+
+#pode ser resolvido usando 'from math import factorial' e usar factorial(x) sendo x o número do fatorial
