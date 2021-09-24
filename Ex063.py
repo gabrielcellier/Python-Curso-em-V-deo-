47ad9e007a85aa56bdcb8687e6524bc46e175eaa
Gabriel Cellier <gabrielcellier@gmail.com>
2021-09-24 17:43:50 -0300

    Exercícios 57-65 (Mundo 2 - aula 14)

--- Ex063.py
+++ Ex063.py
@@ -1,15 +1,14 @@
 #ler um numero 'n' inteiro e mostra na tela os 'n' primeiros elementos da sequencia de fibonacci.
-print ('Calculadora para termos da Sequência de Fibonacci')
-termos = int(input('Quantos termos você quer mostrar: '))
+print ('Aqui mostraremos a Sequência de Fibonacci até o número indicado pelo usuário')
+n = int(input('Digite qual posição na sequência que você quer ver: '))
 t1 = 0
 t2 = 1
 cont = 3
-print ('{} -> {}'.format(t1, t2), end=' ')
-while cont <= termos:
-    t3 = t1 + t2
+print ('{} -> {}'.format(t1, t2), end=' ') #isso é fixo pois t1 = 0 e t2 = 1 sempre.
+while cont <= n:
+    t3 = t1 + t2 #o T seguinte é sempre soma dos 2 T anteriores
     print ('-> {}'.format(t3), end=' ')
-    t1 = t2
+    t1 = t2 #pra continuar migrando a sequencia o t1 vira t2, t2 vira t3 e eles são somados de novo, como no começo
     t2 = t3
     cont += 1
-print('fim!')
 
