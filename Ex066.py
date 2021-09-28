dd5c1a457546dfd563426ce214ac9e7a7167df82
Gabriel Cellier <gabrielcellier@gmail.com>
2021-09-27 23:48:59 -0300

    Exercícios 66-71 (Mundo 2 - aula 15)

---
+++ Ex066.py
@@ -0,0 +1,13 @@
+#programa que leia vários números inteiros. Só para quando digitar '999'.
+#Mostrar quantos números foram digitados a soma deles.
+print ('Digite um número inteiro, mostraremos a soma total deles e quantos números foram digitados!')
+print ('Digite 999 para que o programa seja encerrado')
+qt = 0
+s = 0
+while True:
+    n = int(input('Digite um número inteiro (999 p/ parar): '))
+    if n == 999: #colocar antes para que o 999 não conte no 'qt' e 's'
+        break
+    qt += 1
+    s += n
+print (f'Ao todo foram digitados {qt} números, somando {s} no total.')
