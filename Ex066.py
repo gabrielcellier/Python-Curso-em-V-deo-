ab9f7c004f61c7170139b837f5dbae2a5e697a21
Gabriel Cellier <gabrielcellier@gmail.com>
2021-09-27 23:49:44 -0300

    Exercícios 66-71 (Mundo 2 - aula 15)

--- Ex066.py
+++ Ex066.py
@@ -2,8 +2,7 @@
 #Mostrar quantos números foram digitados a soma deles.
 print ('Digite um número inteiro, mostraremos a soma total deles e quantos números foram digitados!')
 print ('Digite 999 para que o programa seja encerrado')
-qt = 0
-s = 0
+qt = s = 0
 while True:
     n = int(input('Digite um número inteiro (999 p/ parar): '))
     if n == 999: #colocar antes para que o 999 não conte no 'qt' e 's'
