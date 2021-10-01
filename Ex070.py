83246eaa8bc1bf6e0f910c6f1199e425f8660b90
Gabriel Cellier <gabrielcellier@gmail.com>
2021-10-01 18:32:53 -0300

    Exercícios 66-71 (Mundo 2 - aula 15)

--- Ex070.py
+++ Ex070.py
@@ -8,16 +8,18 @@ while True:
     nome = str(input('Digite o nome do produto: ')).capitalize().strip()
     preco = float(input('Digite o preço do produto: R$ '))
     total += preco
-    pbarato = preco
     if preco > 1000:
         p1000 += 1
     if preco < pbarato or pbarato == 0:
+        pbarato = preco
         nbarato = nome
     continuar = str(input('Você deseja continuar a lista (s/n)? ')).strip().lower()[0]
-        if continuar == 'n':
-            break
-    sleep(1)
-print (f'O valor total da lista é R${total}, com {p1000} produtos acima de R$1000.00'
-       f' e com o produto {nbarato} sendo o mais barato')
+    if continuar in 'Nn':
+        break
+    if continuar not in 'NnSs':
+        continuar = str(input('Você deseja continuar a lista (s/n)? ')).strip().lower()[0]
+    sleep(0.3)
+print (f'O valor total da lista é R${total:.2f}, com {p1000} produtos acima de R$1000.00'
+       f' e com o produto {nbarato} sendo o mais barato, custando R${pbarato:.2f}.')
 
 
