21550bff5db397929e090e3c406dd33b4826ec1e
Gabriel Cellier <gabrielcellier@gmail.com>
2021-10-04 19:12:56 -0300

    Exercícios 72-77 (Mundo 3 - aula 16)

--- Ex072.py
+++ Ex072.py
@@ -1,9 +1,11 @@
 #criar tupla com contagem de 0 a 20 por extenso. Usuário deve escolher um número de 0 a 20 e ele aparecer escrito.
 from time import sleep
-lista = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
+lista = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete',
+         'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze',
+         'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 while True:
     n = int(input('Escolha um valor entre 0 e 20: '))
-    if n < 0 or n > 20:
+    if 0 <= n <= 20:      #ou if n < 0 or n > 20
         print (f'Erro! O valor digitado {n} não está entre 0 e 20.')
         n = int(input('Escolha um valor entre 0 e 20: '))
     print (f'O número digitado foi {n}, que por extenso é {lista [n]}.')
