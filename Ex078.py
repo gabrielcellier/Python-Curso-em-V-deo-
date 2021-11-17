18855bf2f4fc8bc9385f852b99eda03b302d0af1
Gabriel Cellier <gabrielcellier@gmail.com>
2021-11-17 05:02:10 -0300

    Exercícios 78-83 (Mundo 3 - aula 17)

--- Ex078.py
+++ Ex078.py
@@ -1,9 +1,19 @@
 #ler 5 valores númericos e guardar numa lista. Mostrar maior e menor valor e suas posicoes na lista
+min = max = posmin = posmax = 0
 lista = []
-for numeros in range(0, 5):
+for n in range(0, 5):     #para se adicionar os 5 valores
     lista.append(int(input('Digite um valor númerico inteiro: ')))
-for c, v in enumerate(lista):
-print(f'A lista foi {lista}')
+    if lista[n] > max or max == 0:   #se o valor for maior que o max definido ou se o max for 0, vai substituir o 'max'
+        max = lista[n]
+    if lista[n] < min or min == 0:   #se o valor for menor que o min definido ou o min for 0, vai substituir o 'min'
+        min = lista[n]
+for i, v in enumerate(lista): #verificando a posição. se o v (valor) for igual o max ou min, logo seu i (índice) é a posição do max ou min
+    if v == max:
+        posmax = i
+    if v == min:
+        posmin = i
+print(f'O maior valor digitado foi {max} na posição {posmax + 1}, o menor foi {min} na posição {posmin + 1}.')
+
 
 
 
