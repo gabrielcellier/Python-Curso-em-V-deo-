01d36c54b01ee18d869430ea4bdfb45d978bfb7c
Gabriel Cellier <gabrielcellier@gmail.com>
2021-11-22 23:02:44 -0300

    Exercícios 84-89 (Mundo 3 - aula 18)

--- Ex085.py
+++ Ex085.py
@@ -1,1 +1,16 @@
-#Escrever 7 números. Dentro da lista já separar os pares e ímpares. No fim mostrar esses valores em ordem crescente.
+#Escrever 7 números. Dentro da lista já separar os pares e ímpares.
+#No fim mostrar esses valores em ordem crescente.
+numeros = [[], []]
+valor = 0
+for n in range (0, 7):
+    valor = int(input(f'Digite o {n+1}° valor: '))
+    if valor % 2 == 0:
+        numeros[0].append(valor)  #p/ add o valor par na primeira lista de números
+    else:
+        numeros[1].append(valor)   #p/ add o valor ímpar na segunda lista de números
+numeros[0].sort()
+numeros[1].sort()
+print(f'Os números pares digitados foram: {numeros[0]}.')
+print(f'Os números ímpares digitados foram: {numeros[1]}.')
+
+
--- Ex086.py
+++ Ex086.py
@@ -1,1 +1,13 @@
 #Perguntar os valores e criar uma matriz (3x3) com os valores dados. Mostrar a matriz ao usuário.
+from time import sleep
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range (0, 3):
+    for c in range (0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+sleep(0.6)
+for l in range (0, 3):
+    for c in range (0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
+
+#nesse método ele cria as listas com valores 0 e substitui eles. poderia criar zerada e ir por append.
