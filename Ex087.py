5ed9c68eaf4aac72c567115c3566b236856aefe1
Gabriel Cellier <gabrielcellier@gmail.com>
2021-11-24 01:25:51 -0300

    Exercícios 84-89 (Mundo 3 - aula 18)

--- Ex087.py
+++ Ex087.py
@@ -6,16 +6,18 @@ soma = somac = 0
 for l in range (0, 3):   #para fazer a matriz
     for c in range (0, 3):
         matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+
 for l in range (0, 3):     #para calcular valores da terceira coluna
     somac += matriz[l][2]  #valores de 3 coluna são sempre c = 2 logo por isso o [2] no lugar de [c]
+print('-*-' * 12)
 for l in range (0, 3):     #para mostrar a matriz
     for c in range (0, 3):
-        print(f'[{matriz[l][c]:^6}]', end='')
+        print(f'[{matriz[l][c]:^5}]', end='')
         if matriz[l][c] % 2 == 0:    #para selecionar os valores pares da matriz
             soma += matriz[l][c]     #os valores pares são adicionados na variável soma
     print()
 print('-*-' * 12)
-sleep(0.5)
+sleep(0.8)
 print(f'A soma de todos os números pares é {soma}.')
 print(f'A soma dos números da terceira coluna é {somac}.')
 print(f'O maior número da segunda linha é {max(matriz[1])}.')
--- Ex088.py
+++ Ex088.py
@@ -1,2 +1,10 @@
 #Perguntar qt de jogos. Fazer jogos de 6 números (não podem se repetir) na qt desejada.
 #Mostrar tudo como lista composta em ordem crescente.
+jogo = []
+for n in range (0, 6):
+    num = int(input(f'Digite o {n+1}º número: '))
+    while num in jogo:
+        num = int(input(f'Número já digitado! Digite um novo número: '))
+    jogo.append(num)
+    cont = str(input())
+
