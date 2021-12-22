a836d9e7bdb33998534b7e6473f0fc5d06e5b2f8
Gabriel Cellier <gabrielcellier@gmail.com>
2021-12-21 21:56:24 -0300

    Exercícios 96-100 (Mundo 3 - aula 20)

--- Ex096.py
+++ Ex096.py
@@ -1,1 +1,13 @@
 #Criar função 'area()' que recebe dimensões de um terreno (largura e comprimento) e mostra a área.
+def area(a, b):
+    print('~*~' * 10)
+    print(f'O terreno tem {l:.2f} x {c:.2f}')
+    a = l * c
+    print(f'Á área total do terreno é de {a:.2f} metros.')
+    print('~*~' * 10)
+
+print('Calculadora de Terreno. Digite a seguir as dimensões do terreno.')
+l = float(input('Digite a largura do terreno (em metros): '))
+c = float(input('Digite o comprimento do terreno (em metros): '))
+
+area(l, c)
--- Ex097.py
+++ Ex097.py
@@ -1,1 +1,9 @@
-#Ter função 'escreva()' pra texto. Mostrar ela com barras (~~~) adaptada pro tamanho pra frase digitada
+# Ter função 'escreva()' pra texto. Mostrar ela com barras (~~~) adaptada pro tamanho pra frase digitada
+def escreva(frase):
+    a = len(frase) + 6
+    print('~' * a)
+    print(f'   {frase}')
+    print('~' * a)
+
+escreva('oioioioi')
+
