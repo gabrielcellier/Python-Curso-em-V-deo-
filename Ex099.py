d607a9fb394f93d232e166f568107874617bfa9f
Gabriel Cellier <gabrielcellier@gmail.com>
2022-01-13 16:28:09 -0300

    Exercícios 96-100 (Mundo 3 - aula 20)

--- Ex099.py
+++ Ex099.py
@@ -2,12 +2,12 @@
 #Analisar os valores e mostrar o maior de todos e a lista toda com contagem de valores
 from time import sleep
 def maior(* num):
-    print(f'Foram informados {len(num)} valores ao todo. São eles:')
+    print(f'Foram informados {len(num)} valores ao todo. São eles:') #contagem de valores na lista
     for a in num:
-        print(a, end=' ', flush=True)
+        print(a, end=' ', flush=True)  #listagem de todos os valores da lista
         sleep(0.4)
     print()
     sleep(0.4)
-    print(f'O maior valor informado foi o {max(num)}.')
+    print(f'O maior valor informado foi o {max(num)}.')  #maior valor na lista
 
 maior(3, 5, 2, 293, 0)
