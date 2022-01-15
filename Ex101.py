99dbbdb4bf7c4156746ed985dcca70a28fe1e0b8
Gabriel Cellier <gabrielcellier@gmail.com>
2022-01-15 16:46:20 -0300

    Exercícios 101-106 (Mundo 3 - aula 21)

--- Ex101.py
+++ Ex101.py
@@ -1,14 +1,17 @@
 #Função chamada voto() que recebe o ano de nascimento da pessoa. Retornar se ela tem que votar ou não
 #ou se o voto é opcional (entre 16-18 anos e acima de 65 anos)
-from datetime import date
 def voto(ano):
+    from datetime import date   #válido colocar dentro se só for ser usado na função
     idade = date.today().year - ano
     if idade < 16:
-        return print(f'Devido a idade de {idade} o voto é PROIBIDO.')
-    if idade >=16 and idade < 18 or idade >= 65:
-        return print(f'Devido a idade de {idade} o voto é OPCIONAL.')
-    if idade >= 18:
-        return print(f'Devido a idade de {idade} o voto é OBRIGATÓRIO.')
+        return f'Na idade de {idade} o voto é PROIBIDO.'
+    elif 16 <= idade < 18 or idade >= 65:
+        return f'Na idade de {idade} o voto é OPCIONAL.'
+    elif idade >= 18:
+        return f'Na idade de {idade} o voto é OBRIGATÓRIO.'
 
 ano = int(input('Digite o ano de nascimento da pessoa: '))
-voto(ano)
+print(voto(ano))
+
+#Não precisa escrever um voto(ano) se eu já jogar ele direto no print, que pegará o resultado com o return na função.
+
