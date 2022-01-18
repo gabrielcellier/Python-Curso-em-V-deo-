73a07dc01cc0f28b694d257730c31c3759e96c2c
Gabriel Cellier <gabrielcellier@gmail.com>
2022-01-18 18:53:22 -0300

    Exercícios 101-106 (Mundo 3 - aula 21)

--- Ex103.py
+++ Ex103.py
@@ -1,2 +1,16 @@
 #Função ficha() recebendo nome e gols de um jogador. Criar uma frase falando que jogador fez x gols.
 #Se não receber nenhum parametro seja nome ou gols, ainda sim criar frase com 'desconhecido' p/ nome e 0 p/ gols.
+def ficha(jogador='desconhecido', gols=0):
+    if jogador == '':
+        jogador == '<desconhecido>'
+    if gols == '':
+        gols == '0'
+    return f'O jogador {jogador} fez {gols} gols na competição.'
+
+j = str(input('Nome do jogador: ')).capitalize().strip()
+g = str(input(f'Gols que o {j} marcou (apenas números): ')).strip()   #usamos str para poder aceitar valor caso usuário escreva por extenso
+if g.isnumeric():    #caso o valor escrito seja númerico mesmo em str, transformaremos em número (int)
+    g = int(g)     #transformando o g numérico de str para int
+else:
+    g = 0  #se tiver escrito por extenso o valor, transformaremos em 0.
+print(ficha(j, g))
