b52090ea8c0b91c189988fcfe69a18b18409ffea
Gabriel Cellier <gabrielcellier@gmail.com>
2022-01-18 21:08:55 -0300

    Exercícios 101-106 (Mundo 3 - aula 21)

--- Ex103.py
+++ Ex103.py
@@ -5,10 +5,10 @@ def ficha(jogador='desconhecido', gols=0):
         jogador == '<desconhecido>'
     if gols == '':
         gols == '0'
-    return f'O jogador {jogador} fez {gols} gols na competição.'
+    return f'O jogador {jogador} fez {gols} gol(s) na competição.'
 
 j = str(input('Nome do jogador: ')).capitalize().strip()
-g = str(input(f'Gols que o {j} marcou (apenas números): ')).strip()   #usamos str para poder aceitar valor caso usuário escreva por extenso
+g = str(input(f'Gols que {j} marcou (apenas números): ')).strip()   #usamos str para poder aceitar valor caso usuário escreva por extenso
 if g.isnumeric():    #caso o valor escrito seja númerico mesmo em str, transformaremos em número (int)
     g = int(g)     #transformando o g numérico de str para int
 else:
--- Ex104.py
+++ Ex104.py
@@ -1,1 +1,14 @@
 #Função leiaint() para receber valor númerico. Enquanto não receber INT dará erro.
+def leiaint(* num):
+    while True:
+        n = str(input('Digite um número: '))
+        if n.isnumeric():
+            n = int(n)
+            break
+        else:
+            print('Erro! Não foi digitado um número.')
+    print(f'O número digitado foi {n}.')
+
+n = leiaint('Digite um número: ')
+
+#Como no ex103, usamos str para pegar o valor e depois fazemos a validaçao de ser um número ou não usando o g.isnumeric()
