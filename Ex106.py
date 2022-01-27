d146b23218f245a0b2caedfae3936b7bdc4cfcaa
Gabriel Cellier <gabrielcellier@gmail.com>
2022-01-27 20:22:32 -0300

    Exercícios 101-106 (Mundo 3 - aula 21)

--- Ex106.py
+++ Ex106.py
@@ -1,17 +1,37 @@
-#Fazer mini-sistema do Interactive Help. O usuário digitará o comando e aparecerá o manual.
-#Se digitar 'fim' se encerra.
-def ajuda(com):
-    help(com)
+# Fazer mini-sistema do Interactive Help. O usuário digitará o comando e aparecerá o manual.
+# Se digitar 'fim' se encerra.
 
-def titulo(msg, cor=0):
-    tam = len(msg)
-    print('~' * tam + 4)
+# lista com índice de cores que serão associadas as funções do interactive help
+c = ('\033[m',  # 0 - sem cor
+     '\033[0;30;41m',  # 1 - vermelho
+     '\033[0;30;42m',  # 2 - verde
+     '\033[0;30;43m',  # 3 - amarelo
+     '\033[0;30;44m',  # 4 - azul
+     '\033[0;30;45m',  # 5 - roxo
+     '\033[7;30m'      # 6 - branco
+     );
+
+
+def ajuda(com):  # função para pegar o valor colocado dentro do () do help()
+    título(f'Acessando o manual da função \'{com}\'', 4)
+    print(cor[6], end='')
+    help(com)
+    print(cor[0], end='')
+
+
+def título(msg, cor=0):  #função para associar as cores a cada uma das funções.
+    tamanho = len(msg)
+    print(c[cor], end='')  # para adicionar a cor no texto abaixo
+    print('~' * tamanho + 4)
     print(f'  {msg}')
-    print('~' * tam + 4)
+    print('~' * tamanho + 4)
+    print(c[0], end='')  # termina a adição de cor
+
 
 comando = ''
 while True:
-    comando = str(input('Função ou Biblioteca > '))
+    título('Sistema de ajuda PyHelp', 1)  # (msg, cor) conforme definido na função 'título()'
+    comando = str(input('Função ou Biblioteca ("fim" p/ sair) >> '))
     if comando.upper() == 'FIM':
         break
     else:
