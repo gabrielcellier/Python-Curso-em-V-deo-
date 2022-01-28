7a412bf55bfe076bdf54ce075bcb535fac12ca88
Gabriel Cellier <gabrielcellier@gmail.com>
2022-01-28 01:26:53 -0300

    Exercícios 101-106 (Mundo 3 - aula 21)

--- Ex101.py
+++ Ex101.py
@@ -1,16 +1,16 @@
 #Função chamada voto() que recebe o ano de nascimento da pessoa. Retornar se ela tem que votar ou não
 #ou se o voto é opcional (entre 16-18 anos e acima de 65 anos)
 def voto(ano):
-    from datetime import date   #válido colocar dentro se só for ser usado na função
+    from datetime import date
     idade = date.today().year - ano
-    if idade < 16:
-        return f'Na idade de {idade} o voto é PROIBIDO.'
+    if idade >= 18:
+        return f'O voto é obrigatório pois a pessoa tem {idade} anos.'
     elif 16 <= idade < 18 or idade >= 65:
-        return f'Na idade de {idade} o voto é OPCIONAL.'
-    elif idade >= 18:
-        return f'Na idade de {idade} o voto é OBRIGATÓRIO.'
+        return f'O voto é opcional pois a pessoa tem {idade} anos.'
+    else:
+        return f'O voto é proibido pois a pessoa tem {idade} anos.'
 
-ano = int(input('Digite o ano de nascimento da pessoa: '))
+ano = int(input('Digite o ano de nascimento para saber a situação da pessoa: '))
 print(voto(ano))
 
 #Não precisa escrever um voto(ano) se eu já jogar ele direto no print, que pegará o resultado com o return na função.
--- Ex102.py
+++ Ex102.py
@@ -1,29 +1,29 @@
 #Criar função fatorial() que recebe o número e o show, que se sim mostra como foi feita a conta.
 #Além disso criar um docstring explicando como funciona a função fatorial.
-def fatorial(a, show=False):
+def fatorial(num, show=False):
     """
-    A função fatorial(a, show=False) serve para calcular o fatorial do número desejado.
-    :param a: O número que o usuário escolhe para ser calculado o fatorial
-    :param show: Por padrão não aparece, se desejado pode-se mostrar o cálculo do fatorial.
+    A função fatorial irá calcular o fatorial do número escolhido pelo usuário.
+    :param num: Número escolhido pelo usuário.
+    :param show: Se verdadeiro, mostrará como o cálculo do número foi efetuado.
+    :return:
     """
     from math import factorial
     from time import sleep
-    resultado = factorial(a)
-    print('---' * 11)
-    print(f'O fatorial do número {a} é {resultado}')
+    calculo = factorial(num)
+    print(f'O cálculo do fatorial de {num} é {calculo}')
     sleep(1)
-    if show == True:
-        print(f'O cálculo do fatorial de {a}:')
-        sleep(1)
-        for c in range(a, 0, -1):   #fazer contador do número escolhido até 1 (calculo fatorial)
-            print(f'{c}', end='')  #Para mostrar sempre o número entre cada x ou =
+    if show:
+        for c in range(num, 0, -1):
+            print(f'{c}', end='')
             if c > 1:
-                print(' x ', end='') #se número for maior que 1, ou seja, não o último da multiplicação, botar x entre eles.
+                print(' x ', end='')
             else:
-                print(' = ', end='') #se número for o 1, ele é o último da multipli. logo botar um = após ele.
-        print(resultado) #finalmente o resultado após todos os n da multiplicação mostrados
-        print('---' * 11)
+                print(' = ', end='')
 
-num = int(input('Digite o número para calcular seu fatorial: '))
-show = bool(input('Você deseja ver o calculo que foi feito (True ou False)? '))
+num = int(input('Qual número você quer calcular o fatorial? '))
+show = str(input(f'Você deseja ver o cálculo do fatorial de {num} (s = sim, n = não): ').lower().strip()[0]
+if show in 's':
+    show=True
+if show in 'n':
+    show=False
 fatorial(num, show)
