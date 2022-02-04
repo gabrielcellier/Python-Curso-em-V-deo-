b0f2d81807313643391f4c5788ca90fca8557c33
Gabriel Cellier <gabrielcellier@gmail.com>
2022-02-03 23:27:18 -0300

    Exercícios 107-112 (Mundo 3 - aula 22)

--- Ex108.py
+++ Ex108.py
@@ -1,1 +1,10 @@
 #pegar o módulo do 107 e adaptar criando a função moeda() que mostre valores como valor monetário formatado.
+from modulos import moeda108
+n = float(input('Digite um valor em reais (R$): '))
+ta = int(input('Digite o valor (em %) de aumento para o valor: '))
+td = int(input('Digite o valor (em %) de diminuição para o valor: '))
+print(moeda108.dobro(n))
+print(moeda108.metade(n))
+print(moeda108.aumentar(n, ta))
+print(moeda108.diminuir(n, td))
+print('---------')
