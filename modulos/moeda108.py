7ae725b2951762f3580f1cbea4038bda6caa03d2
Gabriel Cellier <gabrielcellier@gmail.com>
2022-02-08 00:22:34 -0300

    Exercícios 107-112 (Mundo 3 - aula 22)

--- modulos/moeda108.py
+++ modulos/moeda108.py
@@ -2,22 +2,22 @@ from time import sleep
 def aumentar(n=0, t=0):
     sleep(1)
     print('------')
-    return f'O valor R${n} aumentado em {t}% fica R${t + (n * t/100)}.'
+    return f'O valor R${n} aumentado em {t}% fica R${t + (n * t/100)}'
 
 def diminuir(n=0, t=0):
     sleep(1)
     print('------')
-    return f'O valor R${n} diminuido em {t}% fica R${t - (n * t/100)}.'
+    return f'O valor R${n} diminuido em {t}% fica R${t - (n * t/100)}'
 
 def dobro(n=0):
     sleep(1)
     print('------')
-    return f'O dobro de R${n} é R${n * 2}.'
+    return f'O dobro de R${n} é R${n * 2}'
 
 def metade(n=0):
     sleep(1)
     print('------')
-    return f'Metade de R${n} é R${n / 2}.'
+    return f'Metade de R${n} é R${n / 2}'
 
 def moeda(n=0, moeda='R$'):
-    return f'{moeda}{n:8.2f}'.replace('.', ',')
+    return f'{moeda}{n}'.replace('.', ',')
