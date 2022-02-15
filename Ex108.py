4ce0114b7cb2f9a95591d2ffc822802bd4ff3b86
Gabriel Cellier <gabrielcellier@gmail.com>
2022-02-15 20:42:12 -0300

    Exercícios 107-112 (Mundo 3 - aula 22)

--- Ex108.py
+++ Ex108.py
@@ -3,11 +3,11 @@ from modulos import moeda108
 n = float(input('Digite um valor em reais (R$): '))
 ta = int(input('Digite o valor (em %) de aumento para o valor: '))
 td = int(input('Digite o valor (em %) de diminuição para o valor: '))
-print(f'{moeda108.moeda(moeda108.dobro(n))}.')
-print(f'{moeda108.moeda(moeda108.metade(n))}.')
-print(f'{moeda108.moeda(moeda108.aumentar(n, ta))}.')
-print(f'{moeda108.moeda(moeda108.diminuir(n, td))}.')
-print('---------')
+print(f'A metade de {moeda108.moeda(n)} é {moeda108.moeda(moeda108.metade(n))}.')
+print(f'O dobro de {moeda108.moeda(n)} é {moeda108.moeda(moeda108.dobro(n))}.')
+print(f'O valor {moeda108.moeda(n)} aumentado em {ta}% é {moeda108.moeda(moeda108.aumentar(n, ta))}.')
+print(f'O valor {moeda108.moeda(n)} subtraído em {td}% é {moeda108.moeda(moeda108.diminuir(n, td))}.')
+
 
 #1) O uso do moeda108.moeda() antes das outras funções é para aplicar sua funcionalidade dentro das funções, ou seja,
 #aplicar a formatação com R$ e ',' no lugar de '.'
