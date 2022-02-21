005d4e6b3119be0424b6a2956e482c17f7d921b8
Gabriel Cellier <gabrielcellier@gmail.com>
2022-02-21 20:43:56 -0300

    Exercícios 107-112 (Mundo 3 - aula 22)

--- Ex109.py
+++ Ex109.py
@@ -1,2 +1,4 @@
-#Modificar as funções do 108, aceitando um parametro a mais, essa informado se os valores serão formatados ou não
-#Pela função moeda()
+#Modificar as funções do 108, aceitando um parametro a mais, essa informado se os valores serão formatado
+#ou não pela função moeda()
+from modulos import moeda109
+n =
---
+++ modulos/moeda109.py
@@ -0,0 +1,35 @@
+from time import sleep
+def aumentar(n=0, t=0, formato=False):
+    sleep(1)
+    calculo = (t + (n * t/100))
+    if formato is False:
+        return calculo
+    if formato is True:
+        return moeda(calculo)
+
+def diminuir(n=0, t=0, formato=False):
+    sleep(1)
+    calculo = (t - (n * t/100))
+    if formato is False:
+        return calculo
+    if formato is True:
+        return moeda(calculo)
+
+def dobro(n=0, formato=False):
+    sleep(1)
+    calculo = n * 2
+    if formato is False:
+        return calculo
+    if formato is True:
+        return moeda(calculo)
+
+def metade(n=0, formato=False):
+    sleep(1)
+    calculo = n / 2
+    if formato is False:
+        return calculo
+    if formato is True:
+        return moeda(metade)
+
+def moeda(n=0, moeda='R$'):
+    return f'{moeda}{n}'.replace('.', ',')
