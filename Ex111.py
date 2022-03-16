5ea1f2f7a790815317ec932d49ab1881c79cd250
Gabriel Cellier <gabrielcellier@gmail.com>
2022-03-16 18:28:44 -0300

    Exercícios 107-112 (Mundo 3 - aula 22)

--- Ex111.py
+++ Ex111.py
@@ -1,8 +1,13 @@
-# Criar pacote 'utilidadesCEV' c/ módulos internos 'moeda' e 'dado'. Transferir as funções do 107, 108 e 109
-# p/ o primeiro pacote e tudo funcionar.
-from ex111.utilidadescev import moeda, dado
+# Criar pacote 'utilidadesCEV' c/ módulos internos 'moeda' e 'dado'.
+# Transferir as funções do 107, 108 e 109 p/ o primeiro pacote e tudo funcionar.
+import utilidadescev
+from utilidadescev import moeda
+print('A partir do valor dado (em R$) realizaremos diferentes cálculos.')
 
-p = float(input('Digite um valor em reais: R$ '))
-ta = float(input('Digite uma taxa (em %) para aumento do valor: '))
-td = float(input('Digite uma taxa (em %) para subtração do valor: '))
-moeda.resumo(p, ta, td)
+n = float(input('Digite o valor (em reais) que será usado nos cálculos: R$ '))
+ta = float(input('Digite a taxa em % em quanto deseja aumentar o valor: '))
+td = float(input('Digite a taxa em % em quanto deseja diminuir o valor: '))
+utilidadescev.resumo(n, ta, td)
+
+
+
--- ex111/utilidadescev/dado/__init__.py
+++
@@ -1,6 +0,0 @@
-def leiadinheiro(msg):
-    while True:
-        entrada = str(input(msg)).strip().replace('.', ',')
-        if entrada.isalpha() or entrada == '':
-            print('Erro! Digite um valor válido (valor numérico).')
-
