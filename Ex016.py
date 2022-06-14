dc3cac009d00d620ded5e7650cdbbdcb6ce74ded
Gabriel Cellier <gabrielcellier@gmail.com>
2022-06-14 20:44:30 -0300

    Exercícios 16-21 (Mundo 1 - aula 8)

--- Ex016.py
+++ Ex016.py
@@ -1,9 +1,6 @@
 #Crie um programa que lê um número real qualquer e mostre sua porção inteira ex: 6.7263 parte inteira: 6
-import math
-nq = float(input('Digite um número qualquer: '))
-ni = math.trunc(nq)
-ni2 = math.floor(nq)
-print('O numero dado foi {} e sua porção inteira é {}.'.format(nq, ni2))
-#pode ser usado as variáveis ni ou ni2, bem como já aplicar elas direto no .format()
-#o ni2 pode ser usado pois arredonda o número para baixo
+from math import trunc
+n = float(input('Digite um número real qualquer: '))
+print(f'O número digitado foi {n}, sua porção inteira é {trunc(n)}')
 
+#poderia ser usado math.floor() também, que desce para o primeiro número inteiro
--- Ex017.py
+++ Ex017.py
@@ -1,7 +1,11 @@
 #Dado o cateto oposto e o adjacente, calcule a hipotenusa
 from math import hypot
-co = float(input('Digite o comprimento do cateto oposto: '))
-ca = float(input('Digite o comprimento do cateto adjacente: '))
-hip = hypot(co, ca)
-print('Com o cateto oposto {} e cateto adjacente {}, a hipotenusa do triangulo é {:.2f}.'.format(co, ca, hip))
+from time import sleep
+print('Digite o cateto oposto e o cateto adjacente que calcularemos a hipotenusa do triangulo.')
+sleep(1)
+co = float(input('Digite o cateto oposto: '))
+ca = float(input('Digite o cateto adjacente: '))
+sleep(1)
+print('---------')
+print(f'Com o cateto oposto {co}° e cateto adjacente {ca}° a hipotenusa é {hypot(co, ca):.1f}.')
 
