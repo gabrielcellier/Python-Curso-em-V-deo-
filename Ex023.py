63e2934c97609d5c764d4dc6ff63e89fddb066ef
Gabriel Cellier <gabrielcellier@gmail.com>
2022-06-22 07:13:24 -0300

    Exercícios 22-27 (Mundo 1 - aula 9)

--- Ex023.py
+++ Ex023.py
@@ -1,9 +1,15 @@
 #Ler um n° de 0 a 9999 e mostrar cada digito separado, digito de milhar, centena, dezena e unidade
-numero = int(input('Digite um número entre 0 a 9999: '))
-print ('O numero digitado foi {}'
-       '\nSua unidade é {}.'
-       '\nSua dezena é {}.'
-       '\nSua centena é {}.'
-       '\nSeu milhar é {}.'
-       .format(numero, (numero//1%10), (numero//10%10), (numero//100%10), (numero//1000%10)))
-#poderia também criar uma variável para cada conta e só jogar as variáveis no .format()
+from time import sleep
+print('Digite um número de 0 a 9999. Mostraremos os dígitos separados.')
+sleep(0.5)
+while True:
+       numero = int(input('Digite um numero inteiro entre 0 e 9999: '))
+       if numero >= 0 and numero <= 9999:
+              break
+print(f'O número digitado foi {numero}.'
+      f'\nSua unidade é {numero//1%10}.'
+      f'\nSua dezena é {numero//10%10}.'
+      f'\nSua centena é {numero//100%10}.'
+      f'\nSeu milhar é {numero//1000%10}.')
+
+
