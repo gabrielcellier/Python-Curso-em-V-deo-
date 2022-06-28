bb04ec51d4f9327aa7a4cb56cabb040fa244437a
Gabriel Cellier <gabrielcellier@gmail.com>
2022-06-28 00:40:01 -0300

    Exercícios 36-45 (Mundo 2 - aula 12)

--- Ex036.py
+++ Ex036.py
@@ -1,10 +1,19 @@
 #perguntar I) valor da casa, II) salário do comprador, III) quantos anos pra pagar
 #Calcular o valor da prestação mensal, não pode exceder 30% do salário ou o empréstimo será negado
-casa = float(input('Qual o valor da casa? R$ '))
-salario = float(input('Qual o salário do comprador? R$ '))
-tempo = int(input('Quantos anos para pagar a casa? '))
-prestacao = casa // (tempo * 12)
-if (salario * 0.30) > prestacao:
-    print ('Como o valor da prestação (R${:.2f}) não ultrapassa 30% do salário o empréstimo foi aceito.'.format(prestacao))
+from time import sleep
+print('Digite o valor da casa, o prazo de pagamento e o seu salário. Vamos calculas o valor das prestações.'
+      '\nSe o valor mensal for maior que 30% do seu salário, o empréstimo será negado.')
+sleep(1)
+valor = float(input('Digite o valor da casa: R$ '))
+salario = float(input('Digite o salário do comprador da casa: R$ '))
+prazo = float(input('Prazo de pagamento da casa (em anos): '))
+mensalidade = valor / (prazo * 12)
+sleep(1)
+if mensalidade > (salario * 0.30):
+    print(f'Devido o valor mensal da casa (R$ {mensalidade:.2f}) ser maior que 30% do salário '
+          f'(R$ {(salario * 0.30):.2f}'
+          f'\no empréstimo será negado.')
 else:
-    print ('Como o valor da prestação (R${:.2f}) ultrapassa 30% do salário o empréstimo foi negado.'.format(prestacao))
+    print(f'Devido o valor mensal da casa (R$ {mensalidade:.2f}) ser menor que 30% do salário '
+          f'(R$ {(salario * 0.30):.2f})'
+          f'\no empréstimo será aceito.')
