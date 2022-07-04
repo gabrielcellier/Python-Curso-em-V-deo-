b63facde6bc839a110344eebe13d30b9ff153902
Gabriel Cellier <gabrielcellier@gmail.com>
2022-07-04 20:38:05 -0300

    Exercícios 36-45 (Mundo 2 - aula 12)

--- Ex037.py
+++ Ex037.py
@@ -1,14 +1,28 @@
 #escrever um número inteiro e pedir pro usuário escolher sua base de conversão
 #1 p/ binário, 2 p/ octal e 3 p/ hexadecimal
+from time import sleep
+print('Digite um número. Depois escolha o código correspondente a conversão desejada.')
+sleep(0.8)
 num = int(input('Digite um número inteiro: '))
-print ('''Escolha uma das opções a seguir:
-[ 1 ] transformar o número em binário
-[ 2 ] transformar o número em octal
-[ 3 ] transformar o número em hexadecimal''')
-escolha = int(input('Digite uma opção das oferecidas anteriormente: '))
-if escolha == 1:
-    print ('O número digitado foi {} e sua versão binária é {}.'.format(num, bin(num)[2:]))
-elif escolha == 2:
-    print ('O número digitado foi {} e sua versão octal é {}'.format(num, oct(num)[2:]))
-elif escolha == 3:
-    print ('O número digitado foi {} e sua versão hexadecimal é {}'.format(num, hex(num)[2:]))
+sleep(0.5)
+print('''Escolha o código da conversão desejada:
+[1] Transformar o número em binário
+[2] Transformar o número em octal
+[3] Transformar o número em hexadecimal''')
+while True:
+    cod = int(input('Digite o código (de 1 a 3) da conversão desejada: '))
+    sleep(0.5)
+    if cod in range (1, 4):
+        print(f'O código escolhido foi {cod}.')
+        break
+    else:
+        print(f'O código {cod} é inválido. Digite um código entre 1 e 3.')
+sleep(0.5)
+if cod == 1:
+    print(f'O número {num} transformado em binário fica {bin(num)[2:]}.')
+if cod == 2:
+    print(f'O número {num} transformado em octal fica {oct(num)[2:0]}.')
+if cod == 3:
+    print(f'O número {num} transformado em hexadecimal fica {hex(num)[2:0]}')
+
+
--- Ex038.py
+++ Ex038.py
@@ -1,11 +1,15 @@
 #ler 2 números inteiros e comparar eles, dando mensagem se primeiro for maior, se segundo for maior e
 # se os 2 forem iguais.
-n1 = int(input('Digite o primeiro número inteiro: '))
-n2 = int(input('Digite o segundo número inteiro: '))
+from time import sleep
+print('Digite dois números, vamos comparar eles e falar qual é o maior e o menor.')
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+sleep(0.5)
 if n1 > n2:
-    print ('O primeiro número digitado {} é maior que o segundo {}.'.format(n1, n2))
+    print(f'O primeiro número ({n1}) digitado é maior que o segundo ({n2}).')
 elif n2 > n1:
-    print ('O segundo número digitado {} é maior que o primeiro {}.'.format(n2, n1))
+    print(f'O segundo número ({n2}) digitado é maior que o primeiro ({n1}).')
 else:
-    print ('Os números digitados são iguais e foram ambos {}.'.format(n1))
+    print(f'Os dois números digitados ({n1}) são iguais.')
+
 
