888a725b94240093e1d40c2ff2c3c0cfd28bb588
Gabriel Cellier <gabrielcellier@gmail.com>
2022-07-04 23:44:31 -0300

    Exercícios 36-45 (Mundo 2 - aula 12)

--- Ex039.py
+++ Ex039.py
@@ -1,14 +1,15 @@
 #perguntar ano de nascimento, calcular idade e falar se ta na hora de se alistar, se falta ou se já passou
 #calcular quanto tempo falta ou que já passou de se alistar
 from datetime import date
-ano = int(input('Digite o ano de nascimento da pessoa: '))
-dt = date.today().year
-idade = dt - ano
+from time import sleep
+print('Responda o ano de nascimento, informaremos se já está na hora de participar do alistamento.')
+sleep(0.5)
+ano = int(input('Digite o ano de nascimento: '))
+hoje = date.today().year
+idade = hoje - ano
 if idade == 18:
-    print ('A pessoa completa 18 anos em 2021 logo deve-se alistar.')
+    print(f'Nascidos em {ano} completam 18 anos neste ano. Procure a junta militar próxima para se alistar.')
 elif idade > 18:
-    print ('A pessoa completa {} anos em 2021, logo já passou {} anos do ano de alistamento.'
-           .format(idade, idade - 18))
+    print(f'Nascidos em {ano} fizeram/farão {idade} este ano. Logo passaram {idade - 18} anos do prazo de alistamento.')
 else:
-    print ('A pessoa completa {} anos em 2021, logo faltam {} anos para o seu alistamento.'
-           .format(idade, 18 - idade))
+    print(f'Nascidos em {ano} completam {idade} este ano. Logo faltam {18 - idade} anos do prazo de alistamento.')
--- Ex040.py
+++ Ex040.py
@@ -1,12 +1,29 @@
 #ler duas notas, calcular a média e se for aciam de 7 tá aprovado,
 # entre 5-6.9 recuperação e abaixo de 5 reprovado
-n1 = float(input('Digite a primeira nota: '))
-n2 = float(input('Digite a segunda nota: '))
-media = (n1 + n2) / 2
-if media > 7:
-    print ('A média foi de {:.2f}, logo o aluno foi aprovado!'.format(media))
+from time import sleep
+print('Escreva as duas notas (de 0 a 10), vamos calcular a situação do aluno.'
+      '\nAcima de 7 está aprovado. 5 a 6.9 em recuperação e abaixo de 5 reprovado.')
+sleep(1)
+while True:
+    n1 = float(input('Digite a primeira nota: '))
+    if 0 <= n1 <= 10:
+        print(f'Primeira nota digitada foi: {n1:.2f}.')
+        break
+    else:
+        print(f'Nota digitada {n1:.2f} inválida. Digite uma nota entre 0 a 10.')
+while True:
+    n2 = float(input('Digite a segunda nota: '))
+    if 0 <= n2 <= 10:
+        print(f'Segunda nota digitada foi: {n2:.2f}.')
+        break
+    else:
+        print(f'Nota digitada {n2:.2f} inválida. Digite uma nota entre 0 a 10.')
+media = (n1+n2)/2
+if media >= 7:
+    print(f'A soma das notas {n1:.1f} + {n2:.1f} deu média {media:.1f}. O aluno foi aprovado.')
+elif 5 <= media <= 6.99:
+    print(f'A soma das notas {n1:.1f} + {n2:.1f} deu média {media:.1f}. O aluno está de recuperação.')
 elif media < 5:
-    print ('A média foi de {:.2f}, logo o aluno foi reprovado!'.format(media))
-else:
-    print ('A média foi de {:.2f}, logo o aluno está em recuperação!'.format(media))
+    print(f'A soma das notas {n1:.1f} + {n2:.1f} deu média {media:.1f}. O aluno está reprovado.')
+
 
