529e38507782f9e62d523a8437c2a819b166619d
Gabriel Cellier <gabrielcellier@gmail.com>
2022-07-06 21:58:29 -0300

    Exercícios 46-56 (Mundo 2 - aula 13)

--- Ex046.py
+++ Ex046.py
@@ -1,6 +1,6 @@
-#contagem regressiva de 10 até 0 com 1 segundo entre cada número
+# contagem regressiva de 10 até 0 com 1 segundo entre cada número
 from time import sleep
-for c in range (10, 0, -1):
+print('Vamos fazer contagem regressiva de 10 a 0, diminuindo 1 número a cada segundo.')
+for c in range(10, -1, -1):
     print(c)
     sleep(1)
-print('Foi feito a contagem regressiva de 10 a 0 com intervalo de 1 segundo acima.')
