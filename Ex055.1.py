50f19e366d3e7cbe0496753b2409e9f0798e34f9
Gabriel Cellier <gabrielcellier@gmail.com>
2022-07-18 22:19:01 -0300

    Exercícios 46-56 (Mundo 2 - aula 13)

--- Ex055.1.py
+++ Ex055.1.py
@@ -1,9 +1,20 @@
 #ler peso de 5 pessoas e mostrar qual o maior e o menor
-lista = []
-for p in range (1, 6):
-    peso = float(input('Digite o peso em kg da {}ª pessoa: '.format(p)))
-    lista += [peso]
-print ('O maior peso escrito foi de {:.1f} kg e o menor de {:.1f} kg.'.format(max(lista), min(lista)))
+from time import sleep
+maior = menor = 0
+print('Digite o peso de 5 pessoas, informaremos o menor e maior valor.')
+sleep(1)
+for c in range (1, 6):
+    peso = float(input('Digite o peso (em kg): '))
+    if c == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+    sleep(1)
+print(f'O menor valor de peso foi de {menor:.1f} kg e o maior de {maior:.1f} kgs.')
 
 #nesse modo criamos uma lista que será atualizada com todos os valores de 'peso' inseridos
 #usando max() e min() selecionaremos os maiores e menores valores dessa lista
--- Ex055.py
+++ Ex055.py
@@ -1,7 +1,13 @@
 #ler peso de 5 pessoas e mostrar qual o maior e o menor
 from time import sleep
-maior = menor = 0
+#FEITO PELO MÉTODO DA LISTA. Criamos a lista e pegamos o menor e maior valor.
+lista = []
 print('Digite o peso de 5 pessoas, informaremos o menor e maior valor.')
+sleep(1)
 for c in range (1, 6):
     peso = float(input('Digite o peso (em kg): '))
-
+    lista += [peso]
+    sleep(1)
+print('O peso das 5 pessoas foram computados.')
+sleep(1)
+print(f'O menor peso foi de {min(lista):.1f} kg, o maior de {max(lista):.1f} kg.')
