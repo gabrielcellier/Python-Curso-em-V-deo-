b5e8bd522ad9d15f2a287ae3df480316134ba351
Gabriel Cellier <gabrielcellier@gmail.com>
2022-07-27 20:05:18 -0300

    Exercícios 46-56 (Mundo 2 - aula 13)

--- Ex056.py
+++ Ex056.py
@@ -10,5 +10,49 @@ for c in range (1, 5):
         nome = input('Digite o nome: ').strip().capitalize()
         if nome.isalpha():
             nome = str(nome)
-            print('Resposta válida!')
+            sleep(0.5)
+            break
+        else:
+            print('Resposta inválida. Digite um nome válido.')
+    while True:
+        idade = input('Digite a idade: ')
+        if idade.isnumeric():
+            idade = int(idade)
+            idadetotal += idade
+            sleep(0.5)
+            break
+        else:
+            print('Valor inválido. Digite uma idade com número inteiro.')
             sleep(1)
+    while True:
+        sexo = input('Digite o sexo da pessoa (m/f): ').strip().lower()
+        if sexo in 'mf' and len(sexo) == 1:
+            sexo = str(sexo)
+            sleep(0.5)
+            break
+        else:
+            print('Resposta inválida. Escreva apenas "m" ou "f" para apontar o sexo.')
+    if c == 1 and sexo in 'm': #primeiro homem será o mais velho. mudaremos a partir dele
+        nomehomem = nome
+        idadehomem = idade
+    if sexo in 'm' and idade > idadehomem: #se idade de outro homem for maior que do primeiro, substituirá ele.
+        idadehomem = idade
+        nomehomem = nome
+    if sexo in 'f' and idade < 20: #anotar qt de mulheres menores que 20.
+        qtmulheres += 1
+sleep(1)
+print(f'A média de idade do grupo é de {(idadetotal/4):.1f}.')
+sleep(0.5)
+if nomehomem == '':
+    print(f'Nenhum homem foi inserido. Logo não tem um homem mais velho.')
+else:
+    print(f'O homem mais velho é o {nomehomem} que tem {idadehomem} anos.')
+sleep(0.5)
+if qtmulheres == 0:
+    print(f'Nenhuma mulher foi inserida na lista.')
+elif qtmulheres == 1:
+    print(f'Ao todo, {qtmulheres} mulher tem menos de 20 anos.')
+else:
+    print(f'Ao todo, {qtmulheres} mulheres tem menos de 20 anos.')
+
+
