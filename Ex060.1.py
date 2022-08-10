3bcd021d1bd9031ec3992a52855fd7c7c6be4914
Gabriel Cellier <gabrielcellier@gmail.com>
2022-08-09 22:13:06 -0300

    Exercícios 57-71 (Mundo 2 - aula 14)

--- Ex060.1.py
+++ Ex060.1.py
@@ -1,8 +1,18 @@
 #programa ler um número e mostrar o seu fatorial usando for
-n = int(input('Digite um número para ser calculado o seu fatorial: '))
-f = 1
-c = 1
-for c in range (n,0,-1):
+from time import sleep
+print('Digite um número e calcularemos o seu fatorial.')
+sleep(1)
+f = c = 1
+while True:
+    n = int(input('Digite o número a ser calculado: '))
+    if n.isnumeric():
+        n = int(n)
+        print('Valor válido. Obrigado')
+        break
+    else:
+        print('Valor inválido. Tente novamente.')
+sleep(0.5)
+for c in range(n, 0, -1):
     f *= c
 print ('O fatorial do número digitado {} é {}.'.format(n, f))
 #1) o 'c' vai no N ao 0 diminuindo -1 por vez. o 'f *=c' significa c * (c - 1)
--- Ex060.py
+++ Ex060.py
@@ -1,12 +1,21 @@
 #programa ler um número e mostrar o seu fatorial
-n = int(input('Digite um número para calcularmos seu fatorial: '))
+from time import sleep
+print('Digite um número e iremos calcular o seu fatorial.')
+sleep(1)
+while True:
+    n = int(input('Digite um número: )
+    if n.isnumeric():
+        n = int(n)
+        print('Obrigado. O valor que foi digitado é válido.')
+        break
+    else:
+        print('Valor inválido. Tente Novamente.')
 c = n
 f = 1
 while c > 0:
     f *= c
     c -= 1
-print ('fatorial de {} é {}'.format(n, f))
-
+print(f'O fatorial do valor "{n}" é {f}.')
 #a) nesse modo enquanto C for maior que 0, ele será multiplicado por ele mesmo - 1
 #isso irá ocorrer até que o próprio valor do c chegue a 0
 #obs: fatorial pode ser resolvido usando 'factorial()' da biblioteca math.
