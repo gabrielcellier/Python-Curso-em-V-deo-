a46dda1910c9b853239df4a04ca66a26d7904029
Gabriel Cellier <gabrielcellier@gmail.com>
2022-08-19 20:14:43 -0300

    Exercícios 57-71 (Mundo 2 - aula 14)

--- Ex061.py
+++ Ex061.py
@@ -1,24 +1,25 @@
 #ler o primeiro termo e razão do PA, mostrar os 10 primeiros termos usando while
 from time import sleep
-print('Gerador de Progressão Aritmética. Coloque o 1° termo e razão e calcularemos os 10 próximos termos.')
+print('Digite o primeiro termo e a razão da PA, mostraremos os 10 primeiros termos.')
+print('O programa só trabalha com números inteiros.')
 sleep(1)
 t = r = 0
 while t != "" and r != "":
-    t = input('Digite o primeiro termo da PA: ').strip()
+    t = input('Digite o primeiro termo: ').strip()
     r = input('Digite a razão da PA: ').strip()
     if t.isnumeric() and r.isnumeric():
-        print('Valores válidos! Agora calcularemos a PA.')
         t = int(t)
         r = int(r)
+        print('Valores válidos. Calcularemos a sua PA.')
     else:
-        print('Valores inválidos. Digite um número inteiro como primeiro termo e razão da PA.')
+        print('Valores inválidos. Digite números inteiros.')
         sleep(1)
 sleep(1)
-termo = t
 cont = 1
-while cont <= 10:
-    print(f'{termo}', end=" ")
+while cont <= 10: #contador até 10 pois PA de 10 termos.
+    print(f'{t}', end=' ')
     cont += 1
-    termo += r #adicionado o valor 'r' no termo anterior.
-print(f'\n Acima os 10 primeiros termos da PA com primeiro termo {t} e razão {r}.')
+    t += r #adicionando o valor 'r' no termo anterior.
+print(f'\n Acima os 10 primeiros termos da PA, com primeiro termo {p} e razão {r}.')
+#\n para quebrar a linha depois de usarmos o end=' ' no print()
 
