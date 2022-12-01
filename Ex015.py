b9b1e5067601aebeb7fc60d24c5779789ad44f70
Gabriel Cellier <gabrielcellier@gmail.com>
2022-11-30 23:38:49 -0300

    Exercícios 1-15 (Mundo 1 - aula 2-7)

--- Ex015.py
+++ Ex015.py
@@ -1,8 +1,21 @@
 #perguntas quantos dias ficou com o carro e a km. cobrar 60 por dia + 0,15 por km rodado.
 from time import sleep
-print('O valor do aluguel do carro é de R$60,00 reais por dia e R$0,15 por km rodado.')
+print('Coloque quantos dias o carro será alugado e a km total. Calcularemos o valor a ser pago.')
 sleep(0.5)
-d = int(input('Quantos dias o carro foi/será alugado? '))
-k = float(input('Quantos KM foram/serão rodados com o carro? '))
-print(f'Alugando o carro por {d} dias (R${d*60}) e rodando ao todo {k:.2f} km (R${k*0.15}) '
-      f'o valor do aluguel será de R${((d*60)+(k*0.15)):.2f}.')
+print('A cobrança é de R$60 por dia de aluguel + R$0,15 por km rodado.')
+print('------')
+while True:
+      d = input('Quantos dias o carro será/foi alugado? ')
+      if d.isnumeric():
+            d = int(d)
+            break
+      else:
+            print('Erro. Coloque em valor inteiro o total de dias que o carro foi/será alugado.')
+            sleep(0.5)
+sleep(1)
+k = float(input('Qual foi/será o total de km rodados? '))
+print('-------')
+sleep(1)
+print(f'Ao todo serão {d} dias de aluguel, que custarão R$ {d * 60}, \n'
+      f'{k} km rodados, que custarão R$ {(k * 0.15):.2f}.\n'
+      f'Totalizando assim R$ {((d*60)+(k*0.15)):.2f} o valor a ser pago.')
