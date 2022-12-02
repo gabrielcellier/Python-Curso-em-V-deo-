6410ba02b591f4568e0f099ea8536bd1035b0759
Gabriel Cellier <gabrielcellier@gmail.com>
2022-12-01 21:35:01 -0300

    Exercícios 1-15 (Mundo 1 - aula 2-7)

--- Ex016.py
+++ Ex016.py
@@ -1,6 +1,10 @@
 #Crie um programa que lê um número real qualquer e mostre sua porção inteira ex: 6.7263 parte inteira: 6
 from math import trunc
-n = float(input('Digite um número real qualquer: '))
-print(f'O número digitado foi {n}, sua porção inteira é {trunc(n)}')
+from time import sleep
+print('Digite um número decimal qualquer, mostraremos sua porção inteira.')
+sleep(1)
+n = float(input('Digite um número decimal qualquer: '))
+sleep(1)
+print(f'O número digitado foi {n}, sua porção inteira é {trunc(n)}.')
 
 #poderia ser usado math.floor() também, que desce para o primeiro número inteiro
